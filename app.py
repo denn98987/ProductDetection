baa07825a0d4ef85b9354ca77b4bf9e5ad5260cf
dennis <denn98987@gmail.com>
2021-10-22 13:24:06 +0500

    small server

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask, request
+
+UPLOAD_FOLDER = './'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/', methods=['GET', 'POST'])
+def classify():
+    if request.method == 'POST':
+        if 'image' not in request.files:
+            return 'there is no image in form!'
+        image = request.files['image']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+        image.save(path)
+        return path
+
+        return 'ok'
+    return '''
+    <h1>Upload new File</h1>
+    <form method="post" enctype="multipart/form-data">
+      <input type="file" name="image">
+      <input type="submit">
+    </form>
+    '''
+
+if __name__ == '__main__':
+    app.run()
